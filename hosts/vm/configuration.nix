92ff895f7c2194fab42360aeda50a64d354a7232
TheAccis <accis9683@gmail.com>
2025-12-20 23:42:57 +0300

    .

--- hosts/vm/configuration.nix
+++ hosts/vm/configuration.nix
@@ -1,11 +1,22 @@
 {
 	imports = [
 		../../modules/nixos
-    
+
 		./hardware-configuration.nix
 		./packages.nix
 		./disko.nix
 	];
 
-  virtualisation.qemuGuest.enable = true;
+	services.qemuGuest.enable = true;
+	services.spice-vdagentd.enable = true;
+
+	boot.initrd.availableKernelModules = [
+		"virtio_net"
+		"virtio_pci"
+		"virtio_mmio"
+		"virtio_blk"
+		"virtio_scsi"
+	];
+
+	services.xserver.videoDrivers = [ "virtio_gpu" ];
 }
